14abd80a18d00eca914aa88edffa8bd173fd0c65
KseniiaPan <panovskaia.ksenia74@gmail.com>
2025-03-01 17:52:11 +0300

    добавляет маршрутизацию, страницу продукта и страницу 404

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,27 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import ProductPage from '../../pages/product-page/product-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import {AppRoute} from '../../consts';
 
 function App(): JSX.Element {
   return (
-    <MainPage />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage />}
+        />
+        <Route
+          path={AppRoute.Product}
+          element={<ProductPage />}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
