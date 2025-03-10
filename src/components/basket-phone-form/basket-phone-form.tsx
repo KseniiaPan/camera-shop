9bc7fb8aa49a5a4d4af5ff11e78b516b6dcf9095
KseniiaPan <panovskaia.ksenia74@gmail.com>
2025-03-10 22:10:27 +0300

    добавляет варианты закрытия одального окна, запрет прокрутки

--- src/components/basket-phone-form/basket-phone-form.tsx
+++ src/components/basket-phone-form/basket-phone-form.tsx
@@ -1,37 +1,89 @@
-import {ChangeEvent} from 'react';
+import { useState, useRef } from 'react';
+import { getFormattedPhoneNumber, validatePhone } from '../../utils';
+import { PHONE_INITIAL_VALUE } from '../../consts';
 
 type BasketPhoneFormProps = {
-  onPhoneValueChange: (evt: ChangeEvent<HTMLInputElement>) => void;
-  isValidationError: boolean;
-  phoneNumber: string;
-}
-function BasketPhoneForm({onPhoneValueChange, isValidationError, phoneNumber}:BasketPhoneFormProps): JSX.Element {
+  onModalClose: () => void;
+};
+
+function BasketPhoneForm({ onModalClose }: BasketPhoneFormProps): JSX.Element {
+  const phoneRef = useRef<HTMLInputElement | null>(null);
+  const [phoneNumber, setPhoneNumber] = useState<string>(PHONE_INITIAL_VALUE);
+  const [isValidationError, setIsValidationError] = useState<boolean>(false);
+
+  const handlePhoneValueChange = (value: string | undefined): void => {
+    if (value) {
+      const isPhoneValid = validatePhone(value);
+
+      if (isPhoneValid) {
+        const formattedPhoneNumber = getFormattedPhoneNumber(value);
+        setPhoneNumber(formattedPhoneNumber);
+        setIsValidationError(false);
+      } else {
+        setIsValidationError(true);
+      }
+    }
+  };
+
+  const handleOrderSubmitButton = (
+    evt: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    evt.preventDefault();
+    setPhoneNumber(PHONE_INITIAL_VALUE);
+    setIsValidationError(false);
+    if (phoneRef.current) {
+      phoneRef.current.value = PHONE_INITIAL_VALUE;
+    }
+    onModalClose();
+  };
+
+  const isSubmitButtonDisabled =
+    phoneNumber === PHONE_INITIAL_VALUE || isValidationError;
 
   return (
     <>
       <div className="custom-input form-review__item">
         <label>
           <span className="custom-input__label">
-  Телефон
+            Телефон
             <svg width={9} height={9} aria-hidden="true">
               <use xlinkHref="#icon-snowflake" />
             </svg>
           </span>
           <input
+            ref={phoneRef}
             type="tel"
             name="user-tel"
+            id="phone"
             placeholder="Введите ваш номер"
             defaultValue={phoneNumber}
             minLength={10}
             pattern="^((\+7|8)((\(\d{3}\)|( )?\d{3})( )?)\d{3}(-| )?\d{2}(-| )?\d{2}(-| )?)$"
             data-error-message="Номер должен иметь формат +7(9XX)XXX-XX-XX"
-            onChange={onPhoneValueChange}
+            onChange={() => handlePhoneValueChange(phoneRef.current?.value)}
             required
           />
         </label>
         <p className="custom-input__error">Нужно указать номер</p>
       </div>
-      {isValidationError && <div style={{color: 'red', marginBottom: 15}}>Номер должен иметь формат +7(9XX)XXX-XX-XX</div>}
+      {isValidationError && (
+        <div style={{ color: 'red', marginBottom: 15 }}>
+          Номер должен иметь формат +7(9XX)XXX-XX-XX
+        </div>
+      )}
+      <div className="modal__buttons">
+        <button
+          className="btn btn--purple modal__btn modal__btn--fit-width"
+          type="button"
+          onClick={handleOrderSubmitButton}
+          disabled={isSubmitButtonDisabled}
+        >
+          <svg width={24} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-add-basket" />
+          </svg>
+          Заказать
+        </button>
+      </div>
     </>
   );
 }
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,69 +1,80 @@
-import {ChangeEvent, useState} from 'react';
-import {getFormattedPhoneNumber, validatePhone} from '../../utils';
+import { useEffect, useRef } from 'react';
 import BasketItem from '../../components/basket-item/basket-item';
 import BasketPhoneForm from '../../components/basket-phone-form/basket-phone-form';
-import {useAppSelector} from '../../hooks/index';
-import {getProductsData} from '../../store/product-process/selectors';
-import {ProductModalData} from '../../types/product-types';
-import {PHONE_INITIAL_VALUE} from '../../consts';
+import { useAppSelector } from '../../hooks/index';
+import { getProductsData } from '../../store/product-process/selectors';
+import { ProductModalData } from '../../types/product-types';
 
 type ModalProps = {
   modalData: ProductModalData;
   onModalClose: () => void;
 };
 
-function Modal({onModalClose, modalData}: ModalProps): JSX.Element {
-  const [phoneNumber, setPhoneNumber] = useState<string>(PHONE_INITIAL_VALUE);
-  const [isValidationError, setIsValidationError] = useState<boolean>(false);
+function Modal({ onModalClose, modalData }: ModalProps): JSX.Element {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+
+    const handleClickOutside = (evt: MouseEvent) => {
+      if (
+        (evt.target instanceof HTMLElement &&
+          modalRef.current &&
+          modalRef.current.contains(evt.target))
+      ) {
+        onModalClose();
+      }
+    };
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [modalData.isModalOpen, onModalClose]);
+
+  useEffect(() => {
+    if (!modalData.isModalOpen) {
+      return;
+    }
+
+    const handleEscKeyDown = (evt: KeyboardEvent) => {
+      if (evt.key === 'Escape') {
+        onModalClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscKeyDown);
+    };
+  }, [modalData.isModalOpen, onModalClose]);
+
+  useEffect(() => {
+    if (modalData.isModalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [modalData.isModalOpen]);
 
   const products = useAppSelector(getProductsData);
-  const openedCameraInfo = products.find((product) => modalData.openedCameraId === product.id);
-
-  const handlePhoneValueChange = (evt: ChangeEvent<HTMLInputElement>):void => {
-    const {value} = evt.target;
-    if (value) {
-      const isPhoneValid = validatePhone(value);
-
-      if (isPhoneValid) {
-        const formattedPhoneNumber = getFormattedPhoneNumber(value);
-        setPhoneNumber(formattedPhoneNumber);
-        setIsValidationError(false);
-      } else {
-        setIsValidationError(true);
-      }
-    }
-  };
-
-  const handleOrderSubmitButton = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    evt.preventDefault();
-    setPhoneNumber(PHONE_INITIAL_VALUE);
-    setIsValidationError(false);
-    onModalClose();
-  };
-
-  const isSubmitButtonDisabled = phoneNumber === PHONE_INITIAL_VALUE || isValidationError;
+  const openedCameraInfo = products.find(
+    (product) => modalData.openedCameraId === product.id
+  );
 
   return (
-    <div className={`modal ${modalData.isModalOpen ? 'is-active' : ''}`}>
+    <div
+      className={`modal ${modalData.isModalOpen ? 'is-active' : ''}`}
+      ref={modalRef}
+    >
       <div className="modal__wrapper">
         <div className="modal__overlay" />
         <div className="modal__content">
           <p className="title title--h4">Свяжитесь со мной</p>
-          {openedCameraInfo && <BasketItem openedCameraInfo={openedCameraInfo}/>}
-          <BasketPhoneForm onPhoneValueChange={handlePhoneValueChange} isValidationError={isValidationError} phoneNumber={phoneNumber}/>
-          <div className="modal__buttons">
-            <button
-              className="btn btn--purple modal__btn modal__btn--fit-width"
-              type="button"
-              onClick={handleOrderSubmitButton}
-              disabled={isSubmitButtonDisabled}
-            >
-              <svg width={24} height={16} aria-hidden="true">
-                <use xlinkHref="#icon-add-basket" />
-              </svg>
-            Заказать
-            </button>
-          </div>
+          {openedCameraInfo && (
+            <BasketItem openedCameraInfo={openedCameraInfo} />
+          )}
+          <BasketPhoneForm onModalClose={onModalClose} />
           <button
             className="cross-btn"
             type="button"
