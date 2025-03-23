7086bfad9b17b6fa4440ae72c65dc0a8157a02c2
KseniiaPan <panovskaia.ksenia74@gmail.com>
2025-03-23 22:35:32 +0300

    правки

--- src/components/product-rating/product-rating.tsx
+++ src/components/product-rating/product-rating.tsx
@@ -1,4 +1,4 @@
-import {RATING_STARS} from '../../consts';
+import {MAX_RATING} from '../../consts';
 
 type ProductRatingProps = {
   rating: number;
@@ -7,9 +7,11 @@ type ProductRatingProps = {
 };
 
 function ProductRating({rating, ratingOption, reviewCount}: ProductRatingProps): JSX.Element {
+  const ratingStars = Array.from({length: MAX_RATING}, (_, i) => i + 1);
+
   return (
     <div className={`rate ${ratingOption}__rate`} data-testid="product-rating">
-      {RATING_STARS.map((starValue) => (
+      {ratingStars.map((starValue) => (
         <svg key={starValue} width={17} height={16} aria-hidden="true">
           <use
             xlinkHref={starValue <= rating ? '#icon-full-star' : '#icon-star'}
@@ -19,7 +21,7 @@ function ProductRating({rating, ratingOption, reviewCount}: ProductRatingProps):
       <p className="visually-hidden">Рейтинг: {rating}</p>
       {reviewCount && (
         <p className="rate__count">
-          <span className="visually-hidden">Всего оценок:</span>
+          <span className="visually-hidden">Всего оценок: </span>
           {reviewCount}
         </p>
       )}
--- src/consts.ts
+++ src/consts.ts
@@ -53,12 +53,11 @@ export const SimilarProductsSlider = {
   DefaultFisrstIndex: 0,
   DefaultLastIndex: 3,
   Step: 3,
-};
+} as const;
 
 export const MAX_RATING = 5;
 export const COMMENTS_COUNT_STEP = 3;
 export const COMMENTS_DEFAULT_COUNT = 0;
 
-export const RATING_STARS = Array.from({length: MAX_RATING}, (_, i) => i + 1);
 export const PHONE_REGEXP = /^((\+7|8)((\(\d{3}\)|( )?\d{3})( )?)\d{3}(-| )?\d{2}(-| )?\d{2}(-| )?)$/;
 export const PHONE_INITIAL_VALUE = '';
